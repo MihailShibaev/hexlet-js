cb5e3c72c2b2d597e6c84c7bd924bc94742eab8a
shibaev <shibaev.mihail@hexly.ru>
2022-11-25 16:33:23 +0300

    prettier change files

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import _ from "lodash";
 console.log("Hello, Hexlet!");
 _.intersection([2, 1], [2, 3]); // => [2]
 _.capitalize("hello"); // Hello
-console.log(_.last(['one', 'two']));
+console.log(_.last(["one", "two"]));
